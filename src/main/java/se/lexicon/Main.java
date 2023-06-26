73448f37b8abe21d8d4c0d527d41d4298e4de5f0
Rubotnic <101702711+Rubotnic@users.noreply.github.com>
2023-06-26 15:12:55 +0200

    Update Main.java
    Clean the code and made it better.

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -11,15 +11,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter any year: ");
-        int year = scan.nextInt();
+        int year = scanner.nextInt();
 
         if((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))){
-            System.out.println(year+" is a leap year.");
+            System.out.println(year + " is a leap year.");
         }
         else {
-            System.out.println(year+" is not a leap year.");
+            System.out.println(year + " is not a leap year.");
         }
     }
 }
